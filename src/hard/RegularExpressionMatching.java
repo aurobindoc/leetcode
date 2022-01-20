621d13b15e2d64b94939b9ad782f0c779f06aef0
aurobindoc <aurobindoc1991@gmail.com>
2022-01-20 17:10:30 +0530

    Added solution for RegularExpressionMatching

--- src/hard/RegularExpressionMatching.java
+++ src/hard/RegularExpressionMatching.java
@@ -10,49 +10,37 @@ public class RegularExpressionMatching {
 
     public boolean isMatch(String s, String p) {
 
-        int r = p.length(), c = s.length();
-        if (!p.contains("*") && r !=c ) return false;
+        boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
+        for(int i = 0 ; i < dp.length ; i++){
+            for(int j = 0 ; j < dp[0].length ; j++){
 
-        boolean[][] dp = new boolean[r + 1][c + 1];
-        dp[0][0] = true;
-        for (int i = 1; i <= p.length(); i++) {
-            dp[i][0] = true;
-        }
+                if(i == 0 && j == 0){
+                    dp[i][j] = true;
+                }else if(i == 0){
+                    dp[i][j] = false;
+                }else if(j == 0){
+                    if(p.charAt(i - 1) == '*'){
+                        dp[i][j] = dp[i-2][j];
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else{
 
-        for (int i = 1; i <= p.length(); i++) {
-            for (int j = 1; j < c + 1; j++) {
-
-                if (p.charAt(i - 1) == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    if (p.charAt(i - 1) != '*') {
-                        dp[i][j] = dp[i - 1][j - 1] && p.charAt(i - 1) == s.charAt(j - 1);
-                    } else {
-                        if (p.charAt(i - 2) == '.') dp[i][j] = true;
-                        else if (p.charAt(i - 2) == '*') return false;
-                        else {
-                            if (p.charAt(i - 2) == s.charAt(j - 1)) dp[i][j] = dp[i][j - 1];
-                            else {
-                                dp[i][j] = dp[i - 1][j] || dp[i - 2][j];
-                            }
+                    if(p.charAt(i - 1) == '.' || p.charAt(i-1) == s.charAt(j-1)){
+                        dp[i][j] = dp[i-1][j-1];
+                    }else if(p.charAt(i - 1) == '*'){
+                        dp[i][j] = dp[i-2][j];
+                        if(p.charAt(i-2) == s.charAt(j-1) || p.charAt(i-2) == '.'){
+                            dp[i][j] = dp[i][j] || dp[i][j-1];
                         }
+                    }else{
+                        dp[i][j] = false;
                     }
                 }
-            }
-            System.out.println("=========== " + p.charAt(i - 1) + " ============");
-            printArray(dp);
-            System.out.println("==========================");
-        }
-        return dp[r][c];
-    }
 
-    private static void printArray(boolean[][] arr) {
-        for (boolean[] booleans : arr) {
-            for (boolean b : booleans) {
-                System.out.print(b + "\t");
             }
-            System.out.println();
         }
+        return dp[p.length()][s.length()];
     }
 
     public static void main(String[] args) {
